f4e4ccac4519a1e0416a6d424d9b0774359e98a0
Mahendarreddykommidi <46491974+Mahendarreddykommidi@users.noreply.github.com>
2023-03-30 22:17:35 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <div className="Navigation">
+        <h1><span>Photo </span>graphy </h1>
+      
+		<nav className="Navbar">
+
+
+    
+
+  <li><a href="#">Home</a></li>
+  <li><a href="#">About </a></li>
+  <li><a href="#"> Creators</a></li>
+  <li><a href="#"> Photography</a></li>
+  <li><a href="#"> Support</a> </li>
+</nav>
+<div className="Header">
+  <h1>Iam a photographer </h1>
+  <button className="btn">Hire me </button>
+  </div>
+
+
+     
+    </div>
+
+    </div>
+  );
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-6d9azl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-6d9azl)
